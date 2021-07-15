8cd606a59b5070344f0e93f61e38efd7ab5e5085
torbdahl <53137120+torbdahl@users.noreply.github.com>
2021-07-15 13:31:14 +0200

    Delete __main__.py

--- opendial2/__main__.py
+++
@@ -1,8 +0,0 @@
-from . import system
-import sys
-
-if __name__ == "__main__":
-    args = sys.argv
-    if len(args) < 2:
-        raise RuntimeError("Must specify domain config file")
-    system.DialogueSystem(args[1])
